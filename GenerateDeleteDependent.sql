bfad884396cb2b9a86c96b010850f4a633ca6f85
Jhonny EL HELOU <jhonny.elhelou@gmail.com>
2019-03-02 15:07:29 +0200

    Create GenerateDeleteDependent.sql

---
+++ GenerateDeleteDependent.sql
@@ -0,0 +1,33 @@
+IF EXISTS (SELECT 1 FROM sys.objects where [object_id] = OBJECT_ID('Utility.GenerateDeleteDependent'))
+	DROP FUNCTION [Utility].GenerateDeleteDependent
+GO
+
+CREATE FUNCTION Utility.GenerateDeleteDependent
+(
+	@table nvarchar(100),
+	@Where nvarchar(max)
+)
+RETURNS TABLE 
+AS
+RETURN
+(
+	SELECT * 
+	FROM   (SELECT 'DELETE FROM ' 
+				   + Quotename(Schema_name([table].[schema_id])) + '.' 
+				   + Quotename([table].[name]) + ' WHERE ' 
+				   + Replace(
+				   Stuff( 
+					( 
+						SELECT ' OR ' + Quotename(c.[name]) + ' ' + RTRIM(LTRIM(@Where))
+						FROM sys.foreign_keys fk
+						INNER JOIN sys.foreign_key_columns fkc
+							ON fkc.constraint_object_id = fk.object_id
+						INNER JOIN sys.columns c
+							ON c.object_id = fkc.parent_object_id AND fkc.parent_column_id = c.column_id
+						WHERE fk.parent_object_id = [table].[object_id] AND fk.referenced_object_id = Object_id(@table)
+						FOR XML PATH('')
+					), 1, 4, ''), '&gt;', '>') AS Query 
+			FROM sys.tables [table]) d 
+	WHERE  d.query IS NOT NULL
+)
+GO
